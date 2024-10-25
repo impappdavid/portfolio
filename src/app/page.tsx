8a0450dcf7545ad43318148f1a6e2b3ad90cf9bc
impappdavid <impappdavid@gmail.com>
2024-10-25 13:19:11 +0200

    Create layout structure in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex justify-center mt-14">
       
+      <div className=" w-full max-w-7xl flex">
+          <div className=" w-full max-w-xs h-fit flex ">
+          
+          </div>
+          <div className=" w-full max-w-2xl min-h-screen flex">
+
+          </div>
+
+          <div className=" w-full max-w-xs h-fit flex">
+          
+          </div>
+      </div>
     </div>
   );
 }
