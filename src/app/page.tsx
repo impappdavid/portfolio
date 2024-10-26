769b457311285ac0f6ce7126dae6460e5ebe5a60
impappdavid <impappdavid@gmail.com>
2024-10-26 14:49:37 +0200

    Refine sidebar styles for better user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,9 @@ import { RiTailwindCssFill } from "react-icons/ri";
 import { DiMysql } from "react-icons/di";
 import { IoMdFolder } from "react-icons/io";
 import { IoIosArrowDown } from "react-icons/io";
+import { IoMdHome } from "react-icons/io";
+
+
 
 
 
@@ -23,87 +26,64 @@ export default function Home() {
     <div className=" flex justify-center mt-14">
 
       <div className=" w-full max-w-7xl flex">
-        <div className=" w-full max-w-[250px] h-fit sticky top-14  flex flex-col gap-6 pt-2 ">
-          <ul className="px-2 flex flex-col gap-1">
-            <li className="font-semibold text-sm">Getting started</li>
-            <li className="px-2 text-sm text-zinc-700 dark:text-zinc-400"><a href="" className="hover:text-zinc-900 dark:hover:text-zinc-200">Home</a></li>
-          </ul>
-          <Collapsible defaultOpen={true}>
+        <div className=" w-full max-w-[250px] h-fit sticky top-14  flex flex-col gap-4 pt-2 ">
+          
             <ul className="px-2 flex flex-col gap-1.5">
 
-              <CollapsibleTrigger className="flex justify-between items-center">
-                <li className="font-semibold text-sm">Stack</li>
-                <IoIosArrowDown className="w-3 h-3" />
-              </CollapsibleTrigger>
-              <CollapsibleContent>
-                <li className="px-2 text-sm text-zinc-700 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-200 flex gap-1 items-center hover:cursor-pointer">
+                <li className="font-semibold text-md font-sans">Stack</li>
+             
+                <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-200 flex gap-1 items-center hover:cursor-pointer">
 
-                    <RiNextjsFill className="w-5 h-5" />
-                    NextJS
+                  <RiNextjsFill className="w-5 h-5" />
+                  NextJS
                 </li>
-              </CollapsibleContent>
 
-              <CollapsibleContent>
-                <li className="px-2 text-sm text-zinc-700 dark:text-zinc-400 hover:text-sky-600 dark:hover:text-sky-400 flex gap-1 items-center hover:cursor-pointer">
+                <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-sky-600 dark:hover:text-sky-400 flex gap-1 items-center hover:cursor-pointer">
 
-                    <FaReact className="w-5 h-5" />
-                    React
+                  <FaReact className="w-5 h-5" />
+                  React
                 </li>
-              </CollapsibleContent>
 
-              <CollapsibleContent>
-                <li className="px-2 text-sm text-zinc-700  dark:text-zinc-400 hover:text-orange-600 dark:hover:text-orange-400 flex gap-1 items-center hover:cursor-pointer">
-                    <RiSvelteFill className="w-5 h-5" />
-                    Svelte
+                <li className="font-sans text-md text-zinc-700  dark:text-zinc-400 hover:text-orange-600 dark:hover:text-orange-400 flex gap-1 items-center hover:cursor-pointer">
+                  <RiSvelteFill className="w-5 h-5" />
+                  Svelte
                 </li>
-              </CollapsibleContent>
 
-              <CollapsibleContent>
-                <li className="px-2 text-sm text-zinc-700 dark:text-zinc-400 hover:text-orange-500 dark:hover:text-orange-500 flex gap-1 items-center hover:cursor-pointer">
-                    <BiLogoGit className="w-5 h-5" />
-                    Git
+                <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-orange-500 dark:hover:text-orange-500 flex gap-1 items-center hover:cursor-pointer">
+                  <BiLogoGit className="w-5 h-5" />
+                  Git
                 </li>
-              </CollapsibleContent>
 
-              <CollapsibleContent>
-                <li className="px-2 text-sm text-zinc-700 dark:text-zinc-400 hover:text-blue-600 dark:hover:text-blue-400 flex gap-1 items-center hover:cursor-pointer">
+                <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-blue-600 dark:hover:text-blue-400 flex gap-1 items-center hover:cursor-pointer">
                   <BiLogoTypescript className="w-5 h-5" />
-                    TypeScript
+                  TypeScript
                 </li>
-              </CollapsibleContent>
-              <CollapsibleContent>
-                <li className="px-2 text-sm text-zinc-700 dark:text-zinc-400 hover:text-red-600 dark:hover:text-red-400 flex gap-1 items-center hover:cursor-pointer">
-                  
-                    <SiNestjs className="w-5 h-5" />
-                    NestJS
+                <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-red-600 dark:hover:text-red-400 flex gap-1 items-center hover:cursor-pointer">
+
+                  <SiNestjs className="w-5 h-5" />
+                  NestJS
                 </li>
-              </CollapsibleContent>
-              <CollapsibleContent>
-                <li className="px-2 text-sm text-zinc-700 dark:text-zinc-400 hover:text-sky-600 dark:hover:text-sky-400 flex gap-1 items-center hover:cursor-pointer">
-                  
-                    <DiMysql className="w-5 h-5" />
-                    MySQL
+                <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-sky-600 dark:hover:text-sky-400 flex gap-1 items-center hover:cursor-pointer">
+
+                  <DiMysql className="w-5 h-5" />
+                  MySQL
                 </li>
-              </CollapsibleContent>
-              <CollapsibleContent>
-                <li className="px-2 text-sm text-zinc-700 dark:text-zinc-400 hover:text-sky-600 dark:hover:text-sky-400 flex gap-1 items-center hover:cursor-pointer">
-                  
-                    <RiTailwindCssFill className="w-5 h-5" />
-                    TailwindCSS
+                <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-sky-600 dark:hover:text-sky-400 flex gap-1 items-center hover:cursor-pointer">
+
+                  <RiTailwindCssFill className="w-5 h-5" />
+                  TailwindCSS
                 </li>
-              </CollapsibleContent>
             </ul>
-          </Collapsible>
 
           <Collapsible >
             <ul className="px-2 flex flex-col gap-1.5">
 
-            <CollapsibleTrigger className="flex justify-between items-center">
-                <li className="font-semibold text-sm">Projects</li>
+              <CollapsibleTrigger className="flex justify-between items-center">
+                <li className="font-semibold text-md font-sans">Projects</li>
                 <IoIosArrowDown className="w-3 h-3" />
               </CollapsibleTrigger>
               <CollapsibleContent>
-                <li className="px-2 text-xs text-zinc-700 dark:text-zinc-400 transition-all">
+                <li className="font-sans text-sm text-zinc-700 dark:text-zinc-400 transition-all">
                   Not yet
                 </li>
               </CollapsibleContent>
