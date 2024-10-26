9f1c219c42c163fe3ca1607a8550a90e81192bb8
impappdavid <impappdavid@gmail.com>
2024-10-26 14:56:59 +0200

    Create additional sidebar for in-page navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,53 +27,53 @@ export default function Home() {
 
       <div className=" w-full max-w-7xl flex">
         <div className=" w-full max-w-[250px] h-fit sticky top-14  flex flex-col gap-4 pt-2 ">
-          
-            <ul className="px-2 flex flex-col gap-1.5">
 
-                <li className="font-semibold text-md font-sans">Stack</li>
-             
-                <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-200 flex gap-1 items-center hover:cursor-pointer">
+          <ul className="px-2 flex flex-col gap-1.5">
 
-                  <RiNextjsFill className="w-5 h-5" />
-                  NextJS
-                </li>
+            <li className="font-semibold text-md font-sans">Stack</li>
 
-                <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-sky-600 dark:hover:text-sky-400 flex gap-1 items-center hover:cursor-pointer">
+            <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-200 flex gap-1 items-center hover:cursor-pointer">
 
-                  <FaReact className="w-5 h-5" />
-                  React
-                </li>
+              <RiNextjsFill className="w-5 h-5" />
+              NextJS
+            </li>
 
-                <li className="font-sans text-md text-zinc-700  dark:text-zinc-400 hover:text-orange-600 dark:hover:text-orange-400 flex gap-1 items-center hover:cursor-pointer">
-                  <RiSvelteFill className="w-5 h-5" />
-                  Svelte
-                </li>
+            <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-sky-600 dark:hover:text-sky-400 flex gap-1 items-center hover:cursor-pointer">
 
-                <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-orange-500 dark:hover:text-orange-500 flex gap-1 items-center hover:cursor-pointer">
-                  <BiLogoGit className="w-5 h-5" />
-                  Git
-                </li>
+              <FaReact className="w-5 h-5" />
+              React
+            </li>
 
-                <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-blue-600 dark:hover:text-blue-400 flex gap-1 items-center hover:cursor-pointer">
-                  <BiLogoTypescript className="w-5 h-5" />
-                  TypeScript
-                </li>
-                <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-red-600 dark:hover:text-red-400 flex gap-1 items-center hover:cursor-pointer">
+            <li className="font-sans text-md text-zinc-700  dark:text-zinc-400 hover:text-orange-600 dark:hover:text-orange-400 flex gap-1 items-center hover:cursor-pointer">
+              <RiSvelteFill className="w-5 h-5" />
+              Svelte
+            </li>
 
-                  <SiNestjs className="w-5 h-5" />
-                  NestJS
-                </li>
-                <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-sky-600 dark:hover:text-sky-400 flex gap-1 items-center hover:cursor-pointer">
+            <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-orange-500 dark:hover:text-orange-500 flex gap-1 items-center hover:cursor-pointer">
+              <BiLogoGit className="w-5 h-5" />
+              Git
+            </li>
 
-                  <DiMysql className="w-5 h-5" />
-                  MySQL
-                </li>
-                <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-sky-600 dark:hover:text-sky-400 flex gap-1 items-center hover:cursor-pointer">
+            <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-blue-600 dark:hover:text-blue-400 flex gap-1 items-center hover:cursor-pointer">
+              <BiLogoTypescript className="w-5 h-5" />
+              TypeScript
+            </li>
+            <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-red-600 dark:hover:text-red-400 flex gap-1 items-center hover:cursor-pointer">
 
-                  <RiTailwindCssFill className="w-5 h-5" />
-                  TailwindCSS
-                </li>
-            </ul>
+              <SiNestjs className="w-5 h-5" />
+              NestJS
+            </li>
+            <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-sky-600 dark:hover:text-sky-400 flex gap-1 items-center hover:cursor-pointer">
+
+              <DiMysql className="w-5 h-5" />
+              MySQL
+            </li>
+            <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-sky-600 dark:hover:text-sky-400 flex gap-1 items-center hover:cursor-pointer">
+
+              <RiTailwindCssFill className="w-5 h-5" />
+              TailwindCSS
+            </li>
+          </ul>
 
           <Collapsible >
             <ul className="px-2 flex flex-col gap-1.5">
@@ -93,11 +93,46 @@ export default function Home() {
           </Collapsible>
         </div>
 
-        <div className="w-full max-w-3xl min-h-[2000px] flex bg-red-500/20 mx-auto">
+        <div className="w-full max-w-2xl min-h-[2000px] flex bg-red-500/20 mx-auto">
 
         </div>
 
         <div className=" w-full max-w-[250px] h-fit sticky top-14  flex flex-col gap-6 pt-2 ">
+          <ul className="px-2 flex flex-col gap-1.5">
+
+            <li className="font-semibold text-md font-sans">On This Page</li>
+
+            <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-200 flex gap-1 items-center hover:cursor-pointer">
+              <a href="">
+                Introduction
+              </a>
+            </li>
+
+            <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-200 flex gap-1 items-center hover:cursor-pointer">
+              <a href="">
+                About
+              </a>
+            </li>
+
+            <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-200 flex gap-1 items-center hover:cursor-pointer">
+              <a href="">
+                Education
+              </a>
+            </li>
+
+            <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-200 flex gap-1 items-center hover:cursor-pointer">
+              <a href="">
+                Experience
+              </a>
+            </li>
+
+            <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-200 flex gap-1 items-center hover:cursor-pointer">
+              <a href="">
+                Contact
+              </a>
+            </li>
+
+          </ul>
         </div>
       </div>
     </div>
