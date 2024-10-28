5569e3c61a3be9e244433f129586d8dc8fa37c0d
impappdavid <impappdavid@gmail.com>
2024-10-28 11:40:43 +0100

    Remove unused sidebar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,78 +35,7 @@ export default function Home() {
     <div className=" flex justify-center mt-14">
 
       <div className=" w-full max-w-6xl flex">
-        <div className=" w-full max-w-[250px] h-fit sticky top-14  flex flex-col gap-4 pt-2 ">
-          <BlurFade delay={0.5} inView>
-            <ul className="px-2 flex flex-col gap-1.5">
-
-              <li className="font-semibold text-md font-sans">Stack</li>
-
-              <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-200 flex gap-1 items-center hover:cursor-pointer">
-
-                <RiNextjsFill className="w-5 h-5" />
-                NextJS
-              </li>
-
-              <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-sky-600 dark:hover:text-sky-400 flex gap-1 items-center hover:cursor-pointer">
-
-                <FaReact className="w-5 h-5" />
-                React
-              </li>
-
-              <li className="font-sans text-md text-zinc-700  dark:text-zinc-400 hover:text-orange-600 dark:hover:text-orange-400 flex gap-1 items-center hover:cursor-pointer">
-                <RiSvelteFill className="w-5 h-5" />
-                Svelte
-              </li>
-
-              <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-orange-500 dark:hover:text-orange-500 flex gap-1 items-center hover:cursor-pointer">
-                <BiLogoGit className="w-5 h-5" />
-                Git
-              </li>
-
-              <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-blue-600 dark:hover:text-blue-400 flex gap-1 items-center hover:cursor-pointer">
-                <BiLogoTypescript className="w-5 h-5" />
-                TypeScript
-              </li>
-              <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-red-600 dark:hover:text-red-400 flex gap-1 items-center hover:cursor-pointer">
-
-                <SiNestjs className="w-5 h-5" />
-                NestJS
-              </li>
-              <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-sky-600 dark:hover:text-sky-400 flex gap-1 items-center hover:cursor-pointer">
-
-                <DiMysql className="w-5 h-5" />
-                MySQL
-              </li>
-              <li className="font-sans text-md text-zinc-700 dark:text-zinc-400 hover:text-sky-600 dark:hover:text-sky-400 flex gap-1 items-center hover:cursor-pointer">
-
-                <RiTailwindCssFill className="w-5 h-5" />
-                TailwindCSS
-              </li>
-            </ul>
-          </BlurFade>
-          <BlurFade delay={0.5} inView>
-            <ul className="px-2 flex flex-col gap-1.5">
-
-              <li className=" font-sans ">
-                <a href="" className="w-full flex items-center gap-1">
-                  <div className="font-semibold text-md">
-                    Projects
-                  </div>
-                  <div className="text-xs text-zinc-600 dark:text-zinc-400">
-                    (All)
-                  </div>
-                </a>
-              </li>
-
-              <li className="font-sans text-sm text-zinc-700 dark:text-zinc-400 transition-all">
-                <a href="" className="text-zinc-700 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-200 flex gap-1 items-center hover:cursor-pointer">
-                  <TbFolderFilled className="w-4 h-4" />
-                  DevFlow
-                </a>
-              </li>
-            </ul>
-          </BlurFade>
-        </div>
+        
         <div className="w-full max-w-2xl min-h-[2000px] flex mx-auto">
           <section className="w-full px-4 pt-4 animate-[downblur_2s_ease-in-out] flex flex-col gap-4">
             <div className="grid grid-cols-4">
