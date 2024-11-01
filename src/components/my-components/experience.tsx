f111f237f78bd9081ca35c9cc2fde39f87c1d6be
impappdavid <impappdavid@gmail.com>
2024-11-01 16:49:08 +0100

    .

--- src/components/my-components/experience.tsx
+++ src/components/my-components/experience.tsx
@@ -6,7 +6,7 @@ export default function Experience() {
             <div className=" flex flex-col gap-4 px-4 sm:px-0">
                 <h3 className="text-3xl font-medium font-regularFont ">Experience</h3>
                 <div className="w-full flex justify-center text-xs text-zinc-600 dark:text-zinc-400">
-                    Not yet
+                    Not yet.
                 </div>
             </div>
         </section>
