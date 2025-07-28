64b7d6abd89d77d18c3450f36fa8cf79034ed681
Papp Dávid <impappdavid@gmail.com>
2025-07-29 00:47:20 +0200

    Deleting the page.tsx removed files imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import Hero from "@/components/my-components/explore/hero";
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center bg-zinc-900/70">
-        <div className="lg:w-56 md:w-42"></div>
-        <div className="xl:ml-56 lg:ml-50 md:ml-20 ml-0 px-2 md:px-0 w-full sm:max-w-2xl lg:max-w-3xl xl:max-w-4xl h-fit flex flex-col items-center gap-12 transition-all duration-500" >
-
-
-        </div>
+      <div className="flex flex-col h-screen w-full items-center bg-[#ebe7e1]">
+        
       </div>
     </>
   );
