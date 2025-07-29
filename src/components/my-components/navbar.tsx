3d43951f7c30dadb213530c41c5e5472dedc95e8
Papp Dávid <impappdavid@gmail.com>
2025-07-29 20:32:16 +0200

    Fixing responsiveness

--- src/components/my-components/navbar.tsx
+++ src/components/my-components/navbar.tsx
@@ -14,13 +14,16 @@ function Navbar() {
                     <div className="w-36">
                         <div className=" bg-[#e2404e] w-fit text-white px-2 py-1 font-mono font-semibold cursor-pointer transition-all duration-300 flex items-center">Papp Dávid</div>
                     </div>
-                    <div className="flex gap-4 justify-center">
+                    <div className="hidden sm:flex gap-4 justify-center ">
                         <a href="" className=" hover:underline cursor-pointer ">About</a>
                         <a href="" className=" hover:underline cursor-pointer">Work</a>
                         <a href="" className=" hover:underline cursor-pointer">Project</a>
                     </div>
-                    <div className="w-36 flex justify-end">
+                    <div className="w-36 hidden sm:flex justify-end">
                         <div className="w-fit border-2 border-[#e2404e] font-mono text-[#e2404e] hover:bg-[#e2404e] hover:text-white px-3 py-1 font-semibold cursor-pointer transition-all duration-300">Contact</div>
+                    </div>
+                    <div className="w-36 flex sm:hidden justify-end">
+                        <div className="w-fit border-2 border-[#e2404e] font-mono text-[#e2404e] hover:bg-[#e2404e] hover:text-white px-3 py-1 font-semibold cursor-pointer transition-all duration-300">Menu</div>
                     </div>
                 </div>
             </div>
