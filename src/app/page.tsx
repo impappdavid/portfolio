8c2bd92e5ac73a40814cbccaaab5761cd31724bd
Papp Dávid <impappdavid@gmail.com>
2025-07-29 02:13:15 +0200

    Creating about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import About from "@/components/my-components/about";
 
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col h-screen w-full items-center bg-[#ebe7e1]">
-        
+        <About />
       </div>
     </>
   );
---
+++ src/components/my-components/about.tsx
@@ -0,0 +1,37 @@
+import { Bebas_Neue, Barlow_Condensed } from "next/font/google"
+
+const kodemonoFont = Bebas_Neue({
+    subsets: ["latin"],
+    weight: "400",
+})
+const barlowFont = Barlow_Condensed({
+    subsets: ["latin"],
+    weight: "400",
+})
+
+function About() {
+    return (
+        <>
+            <div className="flex flex-col gap-6 pt-24">
+                <div className={`text-7xl flex justify-center tracking-wider ${kodemonoFont.className}`}>PAPP DÁVID</div>
+                
+                <div className="flex flex-col gap-2 items-center">
+                    <div className="flex flex-col items-center">
+                        <div className={`text-xl tracking-wide ${kodemonoFont.className}`}>ABOUT ME</div>
+                        <div className={`${barlowFont.className} max-w-2xl text-xl text-zinc-600 text-center`}>
+                            I'm a developer <span className="font-semibold">focused on frontend</span> work. I like turning designs into <span className="font-semibold">functional interfaces</span> that are clean and responsive.
+                            I care about writing <span className="font-semibold">clear code</span> and making things that <span className="font-semibold">work well</span> for real people.
+                            Most of my time goes into <span className="font-semibold">learning</span> and building projects that reflect what I value in good software.
+                        </div>
+                    </div>
+                    <div className={`${barlowFont.className} max-w-2xl text-xl text-zinc-600 text-center`}>
+                        Outside of programming, I’ve always found science fascinating. I’ve never studied it in depth, but I think it’s mind-blowing how it explains the world and how things work. The way everything connects from the tiniest particles to the universe itself is something I find endlessly interesting. Even if I don’t always understand the details, I love reading or watching things about physics, space, and how nature works. It reminds me how much there is to explore and how little we still know.
+
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default About
