171f45d4f33a5a3bfc571db5e20a6740dfc068df
Papp Dávid <impappdavid@gmail.com>
2025-08-01 13:05:23 +0200

    Adding page transition

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,18 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "@/components/my-components/navbar";
 import Footer from "@/components/my-components/footer";
+import { ViewTransitions } from "next-view-transitions"
+import CustomCursor from "@/components/my-components/customCursor";
+import { Karla } from 'next/font/google'
 
+const karla = Karla({ subsets: ['latin'], variable: "--font-karla" })
+
+export const metadata1: Metadata = {
+  title: 'Custom Cursor',
+  description: '',
+}
 
 
 export const metadata: Metadata = {
@@ -13,22 +22,23 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <head>
-        <title>Papp Dávid</title>
-      </head>
-      <body
-        className={` antialiased`}
-      >
-        <Navbar />
-        {children}
-        <Footer />
-        <Analytics />
-      </body>
-    </html>
+    <ViewTransitions>
+      <html lang="en">
+        <head>
+          <title>Papp Dávid</title>
+        </head> 
+        <body className={`antialiased ${karla.className}`}>
+          <Navbar />
+          <CustomCursor />
+          {children}
+          <Footer />
+          <Analytics />
+        </body>
+      </html>
+    </ViewTransitions>
   );
 }
