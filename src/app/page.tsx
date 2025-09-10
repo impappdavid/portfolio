56b7919fb792b9350a1b2aac147b68070873b58b
Papp Dávid <impappdavid@gmail.com>
2025-09-10 16:39:18 +0200

    Refactor layout and remove unused styles
    Replaces Sidebar and Navbar with Dock in layout, removes font imports and ThemeProvider, and simplifies body class. Also removes the unused downblur keyframes from globals.css and updates page.tsx to use Content component within ReactLenis.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,9 @@ import ReactLenis from "@studio-freight/react-lenis"
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center bg-zinc-900/70">
-        <div className="lg:w-56 md:w-42"></div>
-        <div className="xl:ml-56 lg:ml-50 md:ml-20 ml-0 px-2 md:px-0 w-full sm:max-w-2xl lg:max-w-3xl xl:max-w-4xl h-fit flex flex-col items-center gap-12 transition-all duration-500" >
-
-          <Hero />
+      <ReactLenis root>
+        <div className="flex flex-col items-center ">
+          <Content />
 
         </div>
       </ReactLenis>
